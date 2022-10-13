75522f758928de456744b629d3ab0168d55d19ec
0skarskiold <oskarskioldlundquist@gmail.com>
2022-10-13 19:14:18 +0200

    fixade lite allt

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-let buttonright = document.getElementById("arrow_right")
-let buttonleft = document.getElementById("arrow_left")
-
-function scrollright(){
-    document.documentElement.scrollTop = 0;
-}
-
-function scrolleft(){
-    document.documentElement.scrollTop = 0;
-}
-
-buttonright.addEventListener('click', scrollright)
-buttonleft.addEventListener('click', scrolleft)
+(function(w){
+    w.addEventListener('load', function(){
+        const   btn_left = document.getElementById('arrow_left'),
+                btn_right = document.getElementById('arrow_right'),
+                content = document.getElementById('list');
+        const content_scroll_width = content.scrollWidth;
+        let content_scoll_left = content.scrollLeft;
+        btn_right.addEventListener('click', () => {
+            content_scoll_left += 245;
+            if (content_scoll_left >= content_scroll_width) { content_scoll_left = content_scroll_width; }
+            content.scrollLeft = content_scoll_left;
+        });
+        btn_left.addEventListener('click', () => {
+            content_scoll_left -= 245;
+            if (content_scoll_left <= 0) {
+                content_scoll_left = 0;
+            }
+            content.scrollLeft = content_scoll_left;
+        });
+    });
+})(window);
