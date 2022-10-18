a63ed4aac6fd92d09b014c7b93db7e2188d880b7
0skarskiold <oskarskioldlundquist@gmail.com>
2022-10-18 14:16:46 +0200

    yooo

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@
         const content_scroll_width = content.scrollWidth;
         let content_scoll_left = content.scrollLeft;
         btn_right.addEventListener('click', () => {
-            content_scoll_left += 245;
+            content_scoll_left += 735;
             if (content_scoll_left >= content_scroll_width) { content_scoll_left = content_scroll_width; }
             content.scrollLeft = content_scoll_left;
         });
         btn_left.addEventListener('click', () => {
-            content_scoll_left -= 245;
+            content_scoll_left -= 735;
             if (content_scoll_left <= 0) {
                 content_scoll_left = 0;
             }
