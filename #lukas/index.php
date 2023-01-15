3a4dc3bdcff78044b18a00b69c2ecc1463575d7c
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2023-01-15 17:05:37 +0100

    index recent activity grejer
    kommer nog behöva kombinera logs och reviews borden

--- #lukas/index.php
+++ #lukas/index.php
@@ -11,30 +11,42 @@
 
     <main>
         <h2><?php echo var_dump($recent); ?></h2>
-        <section class="item_list_section">
-            <button id="btn_l">left</button>
-            <button id="btn_r">right</button>
-            <div class="item_list_container">
-                <ul class="item_list">
-                    <!-- <?php // for($i=1;$i<=20;$i++) {
-                        // echo 
-                        // '<li class="item_container">
-                        //     <div class="block1"></div>
-                        //     <div class="block2"></div>
-                        //     <div class="block3"></div>
-                        // </li>';
-                    // } ?> -->
-                    <?php foreach($recent as $r) {
-                        echo 
-                        '<li class="item_container">
-                            <div class="block1">'.$r['username'].'</div>
-                            <img class="block2" src="/metadata/'.$r['item_type'].'/'.$r['item_uid'].'/'.$r['item_uid'].'.jpg"></img>
-                            <div class="block3"></div>
-                        </li>';
-                    } ?>
-                </ul>
-            </div>
-        </section>
+        <?php if(count($recent) > 0): ?>
+            <section class="item_list_section">
+
+                <button class="scroll l" for="recent" <?php if(count($recent) > 0) { echo "hidden"; } ?> >left</button>
+                <div class="item_list_container" id="recent">
+                    <ul class="item_list">
+                        <?php foreach($recent as $r) {
+                            $i = 0;
+                            $stars = '<div class="activity_stars">';
+                            for($j = $r['rating']; $j > 0; $j -= 0.5) {
+                                if($i % 2 == 0) {
+                                    $stars .= '<div class="activity_halfstar l"></div>';
+                                } else {
+                                    $stars .= '<div class="activity_halfstar r"></div>';
+                                }
+                                $i++;
+                            }
+                            $stars .= '</div>';
+                            echo 
+                            '<li class="activity_container">
+                                <div class="block1"><a href="/users/'.$r['user_uid'].'">'.$r['username'].'</a></div>
+                                <a class="activity_link" href="/users/'.$r['user_uid'].'/item?id='.$r['item_id'].'&entry_type='..'&entry_id='..'">
+                                    <img class="poster" src="/metadata/'.$r['item_type'].'/'.$r['item_uid'].'/'.$r['item_uid'].'.jpg"></img>
+                                    <div class="block2">'.$stars.'</div>
+                                </a>
+                            </li>';
+                        } ?>
+                        <li class="show_more">
+                            <a href="/recent-activity"></a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="scroll r" for="recent" <?php if(count($recent) > 0) { echo "hidden"; } ?> >right</button>
+
+            </section>
+        <?php endif; ?>
     </main>
 
     <?php include_once("sections/footer.php"); ?>
--- #lukas/sections/js.php
+++ #lukas/sections/js.php
@@ -4,20 +4,27 @@
 
     $(document).ready(function() {
 
-        $("#btn_r").click(function() {
-            if(parseInt($('.item_list').css('left')) > -1650 && !$('.item_list').is(':animated')) {
-                $('.item_list').animate({left: "-=550px"});
-                // console.log(parseInt($('.item_list').css('left')));
+        $('button.scroll').click(function() {
+            var list_selector = '#' + $(this).attr('for') + ' .item_list';
+
+            if($(list_selector).length > 0) {
+                var num_children = $(list_selector).children('li').length;
+                var lim = 0;
+                for(let i = num_children; i > 5; i--) {
+                    if(i % 5 == 1) { lim -= 550; }
+                }
+
+                if($(this).hasClass('r')) {
+                    if(parseInt($(list_selector).css('left')) > lim && !$(list_selector).is(':animated')) {
+                        $(list_selector).animate({left: "-=550px"});
+                    }
+                } else if($(this).hasClass('l')) {
+                    if(parseInt($(list_selector).css('left')) < 0 && !$(list_selector).is(':animated')) {
+                        $(list_selector).animate({left: "+=550px"});
+                    }
+                }
             }
         });
-
-        $("#btn_l").click(function() {
-            if(parseInt($('.item_list').css('left')) < 0 && !$('.item_list').is(':animated')) {
-                $('.item_list').animate({left: "+=550px"});
-                // console.log(parseInt($('.item_list').css('left')));
-            }
-        });
-
     });
 
     $(document).on('click', '.follow', function() {
