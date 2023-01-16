8014ece2bcb8f7b329e458fd0761fda0d77d80c7
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2023-01-16 10:13:50 +0100

    entry sida

---
+++ #lukas/entry.php
@@ -0,0 +1,17 @@
+<?php 
+    session_start(); 
+    require_once("includes/dbh.inc.php");
+    require_once("sections/contents.php"); 
+?>
+<body>
+    <?php include_once("sections/header.php"); ?>
+
+    <main>
+
+        <h2>entry</h2>
+
+    </main>
+
+    <?php include_once("sections/footer.php"); ?>
+</body>
+</html>
